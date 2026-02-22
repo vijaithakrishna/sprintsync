0b7e5395380b8ab6426c74920fdd743cbf8b6f30
Vijaithakrishna S Ghosh <vijaithakrishna@gmail.com>
2026-02-22 19:13:17 +0530

    fix: add Netlify URL to CORS allowed origins

--- app/main.py
+++ app/main.py
@@ -23,7 +23,11 @@ app.add_middleware(
 #app.add_middleware(LoggingMiddleware)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://sprintsync-front.netlify.app",
+    ],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
